27e1ac215cda7a453882f7094df9d48f3a46f6d7
Pranav Chand Verma <pranav8991@gmail.com>
2024-10-23 21:39:53 +0530

    Add files via upload

---
+++ 1.Getting Start/Q1.java
@@ -0,0 +1,17 @@
+// Print Z
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");  
+    }
+}
+
+//Time Complexity O(1)
