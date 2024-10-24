584d94e579d001562015cdc36b2074a98b70200f
Pranav Chand Verma <pranav8991@gmail.com>
2024-10-24 23:22:44 +0530

    Add files via upload

---
+++ 3.Function and Arrays/Q3.java
@@ -0,0 +1,37 @@
+//Any Base To Decimal
+
+import java.util.*;
+  
+  public class Main{
+  
+  public static void main(String[] args) {
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
+      int b = scn.nextInt();
+      int dn = getValueInBase(n, b);
+      System.out.println(dn);
+   }
+  
+   public static int getValueInBase(int n, int b){
+    // write code here
+       int p = 1;
+       int ans = 0;
+       while(n>0){
+           int rem = n%10;
+           n = n/10;
+           ans  = ans + rem*p;
+           p = p*b;
+       }
+       return ans;
+   }
+  }
+  
+/*TIME COMPLEXITY :
+
+We are extracting digits of number n and performing some minute calculations, which will take O(log10 n) 
+time as there can be maximum floor(log10 n) digits in a number n.
+
+
+SPACE COMPLEXITY :
+
+Space Complexity - We just need to store a few variables like the answer, multiplier and remainder, hence O(1) auxiliary space is required.*/
